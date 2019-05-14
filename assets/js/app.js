8d718f0e542fdd1c62650696458b1780347cea85
Jorge Pires <jorgepires311@gmail.com>
2019-05-13 19:56:50 -0700

    Session Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 var svg = d3
-  .select(".chart")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
